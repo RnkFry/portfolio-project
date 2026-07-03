a541b0ee88edf8cd247103299d8f0bffab1b2538
Alif Adhitya <alifadhitya13@gmail.com>
2026-07-03 23:20:42 +0700

    Frontend : setup React Router

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-600">
-        Hello Portfolio 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
